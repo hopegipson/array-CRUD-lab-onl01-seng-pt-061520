53bfe6c7806dcc5dde6fe2f2d95fc4373e2567f7
Hope Gipson <gipson.hope@gmail.com>
2020-06-22 02:32:34 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -16,12 +16,12 @@ def add_element_to_start_of_array(array, element)
 end
 
 def remove_element_from_end_of_array(array)
-  
+end_array = array.pop
+
 end
 
 def remove_element_from_start_of_array(array)
-  end_array = array.pop
-  returns end_array
+  
 end
 
 def retrieve_element_from_index(array, index_number)
